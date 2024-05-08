e8b7423c06956f1aa519ca59b179b06b12272483
Helene Goyat <goyat.helene@gmail.com>
2024-05-07 19:02:34 -0600

    Main page

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,14 @@
+<script setup lang="ts">
+import NavBar from "@/components/Layout/NavBar.vue";
+import FooterBar from "@/components/Layout/FooterBar.vue";
+</script>
+
 <template>
+  <nav-bar />
   <router-view></router-view>
+  <footer-bar />
 </template>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Comfortaa:wght@700&family=Days+One&family=Kumbh+Sans:wght@100..900&family=Pacifico&display=swap");
+</style>
---
+++ src/components/Layout/NavBar.vue
@@ -0,0 +1,55 @@
+<template>
+  <nav class="bg-white sticky w-full z-20 top-0 start-0 border-b border-gray-200">
+    <div class="max-w-screen-lg flex flex-wrap items-center justify-between mx-auto p-4">
+      <a
+        href="https://flowbite.com/"
+        class="flex items-center space-x-3 rtl:space-x-reverse">
+        <span class="brand text-purple-700 self-center text-3xl whitespace-nowrap">Haloonie</span>
+      </a>
+      <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+        <button
+          type="button"
+          class="text-white bg-emerald-400 hover:bg-emerald-500 font-medium rounded-full text-sm px-4 py-2 text-center">
+          Get started
+        </button>
+        <button
+          data-collapse-toggle="navbar-sticky"
+          type="button"
+          class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+          aria-controls="navbar-sticky"
+          aria-expanded="false">
+          <span class="sr-only">Open main menu</span>
+          <svg
+            class="w-5 h-5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 17 14">
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M1 1h15M1 7h15M1 13h15" />
+          </svg>
+        </button>
+      </div>
+      <div
+        class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+        id="navbar-sticky">
+        <ul
+          class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white">
+          <li
+            v-for="item in ['About', 'Our Boxes', 'Specials']"
+            :key="item">
+            <a
+              href="#"
+              class="block py-2 px-3 text-gray-900 rounded hover:bg-violet-300 md:hover:bg-transparent hover:text-violet-700 md:p-0">
+              {{ item }}
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
