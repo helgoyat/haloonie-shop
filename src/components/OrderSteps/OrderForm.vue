b9538de80200dc7b771cb16c2ad14248d1867f3f
Helene Goyat <goyat.helene@gmail.com>
2024-05-10 20:17:05 -0600

    Keep

--- src/components/OrderSteps/OrderForm.vue
+++ src/components/OrderSteps/OrderForm.vue
@@ -2,11 +2,39 @@
 
 <template>
   <form class="max-w-2xl w-full">
+    <div class="grid md:grid-cols-2 md:gap-6">
+      <div class="mb-5">
+        <label
+          for="email"
+          class="block mb-2 text-sm font-medium text-gray-900">
+          First Name
+        </label>
+        <input
+          type="email"
+          id="email"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-violet-300 focus:border-violet-300 block w-full p-2.5"
+          placeholder="name@flowbite.com"
+          required />
+      </div>
+      <div class="mb-5">
+        <label
+          for="email"
+          class="block mb-2 text-sm font-medium text-gray-900">
+          Last Name
+        </label>
+        <input
+          type="email"
+          id="email"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-violet-300 focus:border-violet-300 block w-full p-2.5"
+          placeholder="name@flowbite.com"
+          required />
+      </div>
+    </div>
     <div class="mb-5">
       <label
         for="email"
         class="block mb-2 text-sm font-medium text-gray-900">
-        Your email
+        Email
       </label>
       <input
         type="email"
