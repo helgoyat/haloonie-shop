8a3a00b3f9a1b7cb4dd11f2b1956eea423299152
Helene Goyat <goyat.helene@gmail.com>
2024-05-13 17:50:46 -0600

    Keep

--- src/data/collection-boxes.ts
+++ src/data/collection-boxes.ts
@@ -16,6 +16,68 @@ export const CollectionBoxes: IBox[] = [
     name: "Pepito",
     image: "https://cdn.worldvectorlogo.com/logos/pepito.svg",
     price: 78,
-    cookies: { "6c428180-a080-4c55-a0be-bfeb2d98134a": 1 },
+    cookies: {
+      "163746cb-fc56-4581-8039-3cf8b0e7bb5c": 1,
+      "fbb6ad4d-6559-4018-b66f-c56020061650": 1,
+    },
+  },
+  {
+    id: "c5bf8a6b-770f-411f-956a-b9945dc6a9c2",
+    name: "Granola",
+    image:
+      "https://www.mavieencouleurs.fr/sites/default/files/2021-04/335316-GRLA-LOGO-Q1-2020.png",
+    price: 30,
+    cookies: {
+      "8da70bfb-6539-4fe3-a21b-1281808d3a96": 1,
+      "017848a2-581e-41f3-be44-e4356a54b6af": 1,
+      "a6efb036-8b64-488f-b7f3-e5cf9f86c5ec": 1,
+      "02fd05e1-344a-4945-a99f-cf8b8aae1309": 1,
+      "925848f2-befd-43af-bc95-bd8850777c56": 1,
+    },
+  },
+  {
+    id: "29b9c25f-d220-48d4-b7a7-dbf41312bfd1",
+    name: "Prince",
+    image: "https://lemediacom.com/wp-content/uploads/2022/05/LU_Prince-logo-2.png",
+    price: 54,
+    cookies: {
+      "03b47f63-efea-460d-a24e-406aae52b549": 1,
+      "c1bf2eb5-c13f-456e-9524-678b20210c6e": 1,
+    },
+  },
+  {
+    id: "9aa04c84-5ec8-4c70-a644-03e7ec6688e9",
+    name: "Bn",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgdhkFRpTGR6V75XQuL5lJ111KXiNQSf1pugUYYlxslw&s",
+    price: 62,
+    cookies: {
+      "03d8982b-a05b-49c3-a150-c6908f444033": 1,
+      "cd63002d-04b0-49cc-b16a-7e2d58af9d82": 1,
+    },
+  },
+  {
+    id: "068f549a-2a7e-4b43-8b17-3272932c8992",
+    name: "St Michel",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAdezQrFMkABMTkzG0z0Ss3UUJuxALyrtSIqQXbYLdgQ&s",
+    price: 40,
+    cookies: {
+      "e8f9491e-1ebc-45d7-9a61-5a094c23a60f": 1,
+      "9331ad12-29ae-47f2-ae6b-a4af63adaa67": 1,
+      "bf09553c-8307-4f8b-aaec-080fa92c8683": 1,
+    },
+  },
+  {
+    id: "b20dbfa2-db7d-48b8-84f0-611d5f3b68f6",
+    name: "Bonne Maman",
+    image: "https://cdn.worldvectorlogo.com/logos/bonne-maman.svg",
+    price: 46,
+    cookies: {
+      "82d9d326-ad7d-4cdd-a61f-db31e4b69900": 1,
+      "6847c9a7-40f6-4ad9-b79a-6bbecdd2a7a9": 1,
+      "3b6faf3a-a99a-4860-9237-ff505fcbba42": 1,
+      "1c9005fb-e165-4a5e-aabf-f1dd286f7d19": 1,
+    },
   },
 ];
--- src/data/cookies.ts
+++ src/data/cookies.ts
@@ -3,7 +3,7 @@ import { ICookie } from "@/types";
 export const Cookies: ICookie[] = [
   {
     id: "f50f9320-f33e-4ede-8582-c3500532f98c",
-    name: "Barre nappés de chocolat au lait Petit Ecolier",
+    name: "Biscuits barre nappés de chocolat au lait Petit Ecolier",
     brand: "LU",
     image:
       "https://www.coursesu.com/dw/image/v2/BBQX_PRD/on/demandware.static/-/Sites-digitalu-master-catalog/default/dwdb7ff4d0/3017760002905_H1N1_800367_S10.png?sw=250&sh=250&sm=fit",
@@ -11,7 +11,7 @@ export const Cookies: ICookie[] = [
   },
   {
     id: "c2ae0379-9801-4e6c-a842-069fc78c4c49",
-    name: "Baiocchi Snack fourrés aux noisettes & cacao",
+    name: "Biscuits Baiocchi Snack fourrés aux noisettes & cacao",
     brand: "Mulino Bianco",
     image:
       "https://www.coursesu.com/dw/image/v2/BBQX_PRD/on/demandware.static/-/Sites-digitalu-master-catalog/default/dw02cee235/8076809517706_A1L1_2312100_S12.png?sw=250&sh=250&sm=fit",
@@ -19,7 +19,7 @@ export const Cookies: ICookie[] = [
   },
   {
     id: "90d80965-c02f-4772-9e68-eedd864e0d2a",
-    name: "Dinosaurus chocolat noir",
+    name: "Biscuits dinosaurus chocolat noir",
     brand: "Lotus",
     image:
       "https://www.coursesu.com/dw/image/v2/BBQX_PRD/on/demandware.static/-/Sites-digitalu-master-catalog/default/dw771729a3/5410126004021_A_7130141_S01.png?sw=250&sh=250&sm=fit",
@@ -27,7 +27,7 @@ export const Cookies: ICookie[] = [
   },
   {
     id: "8da70bfb-6539-4fe3-a21b-1281808d3a96",
-    name: "Sablés nappés au chocolat au lait",
+    name: "Biscuits sablés nappés au chocolat au lait",
     brand: "Granola",
     image:
       "https://www.coursesu.com/dw/image/v2/BBQX_PRD/on/demandware.static/-/Sites-digitalu-master-catalog/default/dw00863372/3017760635295_A1L1_4364861_S12.png?sw=250&sh=250&sm=fit",
@@ -35,7 +35,7 @@ export const Cookies: ICookie[] = [
   },
   {
     id: "523edc74-9902-4bea-8747-b8c91b3e9b6d",
-    name: "Petit beurre chocolat noir Petit Ecolier",
+    name: "Biscuits petit beurre chocolat noir Petit Ecolier",
     brand: "LU",
     image:
       "https://www.coursesu.com/dw/image/v2/BBQX_PRD/on/demandware.static/-/Sites-digitalu-master-catalog/default/dwe9fda129/3017760002035_H1N1_92355_S10.png?sw=250&sh=250&sm=fit",
@@ -43,7 +43,7 @@ export const Cookies: ICookie[] = [
   },
   {
     id: "e8f9491e-1ebc-45d7-9a61-5a094c23a60f",
-    name: "Tronches de Cake",
+    name: "Biscuits Tronches de Cake",
     brand: "St Michel",
     image:
       "https://www.coursesu.com/dw/image/v2/BBQX_PRD/on/demandware.static/-/Sites-digitalu-master-catalog/default/dwc1ef13ff/3178530405804_A1L1_7009259_S12.png?sw=250&sh=250&sm=fit",
@@ -51,7 +51,7 @@ export const Cookies: ICookie[] = [
   },
   {
     id: "565db74a-a6bb-4938-8847-8486c6ce1380",
-    name: "Feuilletés caramélisés Palmito",
+    name: "Biscuits feuilletés caramélisés Palmito",
     brand: "LU",
     image:
       "https://www.coursesu.com/dw/image/v2/BBQX_PRD/on/demandware.static/-/Sites-digitalu-master-catalog/default/dw74c8689f/3017760030106_H1N1_21350_S10.png?sw=250&sh=250&sm=fit",
@@ -59,7 +59,7 @@ export const Cookies: ICookie[] = [
   },
   {
     id: "bc47b267-7b1f-4a53-9ee4-7b2a892002a5",
-    name: "Gâteau KINDER cards",
+    name: "Gâteau Kinder cards",
     brand: "Kinder",
     image:
       "https://www.coursesu.com/dw/image/v2/BBQX_PRD/on/demandware.static/-/Sites-digitalu-master-catalog/default/dwe342ba8a/8000500269169_A1L1_5231731_S12.png?sw=250&sh=250&sm=fit",
@@ -75,7 +75,7 @@ export const Cookies: ICookie[] = [
   },
   {
     id: "6c428180-a080-4c55-a0be-bfeb2d98134a",
-    name: "Fourrés au chocolat au lait Croc Sablé Pepito",
+    name: "Biscuits fourrés au chocolat au lait Croc Sablé Pepito",
     brand: "LU",
     image:
       "https://www.coursesu.com/dw/image/v2/BBQX_PRD/on/demandware.static/-/Sites-digitalu-master-catalog/default/dwedcdc7ca/7622210400574_H1N1_7613881_S10.png?sw=250&sh=250&sm=fit",
@@ -83,7 +83,7 @@ export const Cookies: ICookie[] = [
   },
   {
     id: "ba6127e1-d80e-4727-9447-f561984497bd",
-    name: "Génoises nappées framboise chocolat Pim's",
+    name: "Biscuits génoises nappées framboise chocolat Pim's",
     brand: "LU",
     image:
       "https://www.coursesu.com/dw/image/v2/BBQX_PRD/on/demandware.static/-/Sites-digitalu-master-catalog/default/dw2566ccb5/5410041040807_H1N1_167622_S10.png?sw=250&sh=250&sm=fit",
@@ -91,17 +91,186 @@ export const Cookies: ICookie[] = [
   },
   {
     id: "03b47f63-efea-460d-a24e-406aae52b549",
-    name: "Petit déjeuner aux céréales PRINCE",
+    name: "Biscuits petit déjeuner aux céréales Prince",
     brand: "LU",
     image:
       "https://www.coursesu.com/dw/image/v2/BBQX_PRD/on/demandware.static/-/Sites-digitalu-master-catalog/default/dw711ff465/3017760306492_H1N1_142980_S10.png?sw=250&sh=250&sm=fit",
     active: true,
   },
+  {
+    id: "84e519f1-0e7f-4155-8509-d8b9badb659c",
+    name: "Biscuits fourrés à l'orange Chamonix",
+    brand: "LU",
+    image:
+      "https://www.coursesu.com/dw/image/v2/BBQX_PRD/on/demandware.static/-/Sites-digitalu-master-catalog/default/dw789e9f17/3017760819121_H1N1_20958_S10.png?sw=250&sh=250&sm=fit",
+    active: false,
+  },
+  {
+    id: "017848a2-581e-41f3-be44-e4356a54b6af",
+    name: "Biscuits mini aux pépites de chocolat",
+    brand: "Granola",
+    image:
+      "https://www.coursesu.com/dw/image/v2/BBQX_PRD/on/demandware.static/-/Sites-digitalu-master-catalog/default/dwc79f38f8/7622210420213_H1N1_1268965_S10.png?sw=250&sh=250&sm=fit",
+    active: true,
+  },
+  {
+    id: "c1bf2eb5-c13f-456e-9524-678b20210c6e",
+    name: "Biscuits fourrés au chocolat au blé complet",
+    brand: "LU",
+    image:
+      "https://www.coursesu.com/dw/image/v2/BBQX_PRD/on/demandware.static/-/Sites-digitalu-master-catalog/default/dwfa805be9/5410041423600_H1N1_1425685_S10.png?sw=250&sh=250&sm=fit",
+    active: true,
+  },
+  {
+    id: "fbb6ad4d-6559-4018-b66f-c56020061650",
+    name: "Biscuits barre au chocolat et au riz souflé Pepito",
+    brand: "LU",
+    image:
+      "https://www.coursesu.com/dw/image/v2/BBQX_PRD/on/demandware.static/-/Sites-digitalu-master-catalog/default/dw20bb4cce/3017760465496_H1N1_4178262_S10.png?sw=250&sh=250&sm=fit",
+    active: true,
+  },
+  {
+    id: "03d8982b-a05b-49c3-a150-c6908f444033",
+    name: "Mini biscuits fourrés chocolat",
+    brand: "BN",
+    image:
+      "https://www.coursesu.com/dw/image/v2/BBQX_PRD/on/demandware.static/-/Sites-digitalu-master-catalog/default/dw2d9849b6/3061990141101_A1L1_4355277_S12.png?sw=250&sh=250&sm=fit",
+    active: true,
+  },
+  {
+    id: "cd63002d-04b0-49cc-b16a-7e2d58af9d82",
+    name: "Mini biscuits fourrés goût fraise",
+    brand: "BN",
+    image:
+      "https://www.coursesu.com/dw/image/v2/BBQX_PRD/on/demandware.static/-/Sites-digitalu-master-catalog/default/dw48a04fef/3061990141118_A1L1_1315795_S12.png?sw=250&sh=250&sm=fit",
+    active: true,
+  },
+  {
+    id: "7e8fac33-2561-433f-a020-b9162b7f17d3",
+    name: "Biscuits au chocolat au lait Choco'Croc Petits Coeurs",
+    brand: "LU",
+    image:
+      "https://www.coursesu.com/dw/image/v2/BBQX_PRD/on/demandware.static/-/Sites-digitalu-master-catalog/default/dw1fbcd401/3056440212494_H1N1_562734_S10.png?sw=250&sh=250&sm=fit",
+    active: true,
+  },
+  {
+    id: "9331ad12-29ae-47f2-ae6b-a4af63adaa67",
+    name: "Gâteau avec nappage au chocolat en poudre doomino",
+    brand: "St Michel",
+    image:
+      "https://www.coursesu.com/dw/image/v2/BBQX_PRD/on/demandware.static/-/Sites-digitalu-master-catalog/default/dw1695d5a2/3178530421194_A1L1_2562252_S12.png?sw=250&sh=250&sm=fit",
+    active: true,
+  },
+  {
+    id: "a6efb036-8b64-488f-b7f3-e5cf9f86c5ec",
+    name: "Biscuits barre cookie aux pépites de chocolat",
+    brand: "Granola",
+    image:
+      "https://www.coursesu.com/dw/image/v2/BBQX_PRD/on/demandware.static/-/Sites-digitalu-master-catalog/default/dw5c251217/7622210701336_H1N1_5777400_S10.png?sw=250&sh=250&sm=fit",
+    active: true,
+  },
+  {
+    id: "82d9d326-ad7d-4cdd-a61f-db31e4b69900",
+    name: "Biscuits Tartelettes chocolat au lait caramel",
+    brand: "Bonne Maman",
+    image:
+      "https://www.coursesu.com/dw/image/v2/BBQX_PRD/on/demandware.static/-/Sites-digitalu-master-catalog/default/dw22d2da0e/3178530403022_A1L1_2805784_S12.png?sw=250&sh=250&sm=fit",
+    active: true,
+  },
+  {
+    id: "18e53529-43cc-4832-85fd-4049b1772766",
+    name: "Biscuits Kinder Duo chocoalt lait et blanc",
+    brand: "Kinder",
+    image:
+      "https://www.coursesu.com/dw/image/v2/BBQX_PRD/on/demandware.static/-/Sites-digitalu-master-catalog/default/dw67ecce93/8000500385807_H1L1_6243892_S10.png?sw=250&sh=250&sm=fit",
+    active: true,
+  },
+  {
+    id: "87ee42f0-fed7-4b40-9c77-23397b315e6d",
+    name: "Gâteau chocolat au lait Delichoc",
+    brand: "Delichoc",
+    image:
+      "https://www.coursesu.com/dw/image/v2/BBQX_PRD/on/demandware.static/-/Sites-digitalu-master-catalog/default/dw71614dfa/3116430218483_A1L1_6646900_S12.png?sw=250&sh=250&sm=fit",
+    active: true,
+  },
+  {
+    id: "bf09553c-8307-4f8b-aaec-080fa92c8683",
+    name: "Doonuts nappés chocolat",
+    brand: "St Michel",
+    image:
+      "https://www.coursesu.com/dw/image/v2/BBQX_PRD/on/demandware.static/-/Sites-digitalu-master-catalog/default/dw6d4510c4/3178530414370_H1N1_3862210_S10.png?sw=250&sh=250&sm=fit",
+    active: true,
+  },
+  {
+    id: "163746cb-fc56-4581-8039-3cf8b0e7bb5c",
+    name: "Biscuits fourrés au chocolat au lait Mini Rollos Pepito",
+    brand: "LU",
+    image:
+      "https://www.coursesu.com/dw/image/v2/BBQX_PRD/on/demandware.static/-/Sites-digitalu-master-catalog/default/dw4c67dbad/3017760589895_H1L1_2291564_S10.png?sw=250&sh=250&sm=fit",
+    active: true,
+  },
+  {
+    id: "bb05147f-731a-49a7-bcc4-f8f5d279f30d",
+    name: "La galette fourrée chocolat au lait",
+    brand: "St Michel",
+    image:
+      "https://www.coursesu.com/dw/image/v2/BBQX_PRD/on/demandware.static/-/Sites-digitalu-master-catalog/default/dwb90718cc/3178530423150_A1L1_3949152_S12.png?sw=250&sh=250&sm=fit",
+    active: false,
+  },
+  {
+    id: "7db5a204-1f51-4b47-8d85-83b2e8017526",
+    name: "Biscuits Twix top",
+    brand: "Twix",
+    image:
+      "https://www.coursesu.com/dw/image/v2/BBQX_PRD/on/demandware.static/-/Sites-digitalu-master-catalog/default/dw1c1c0c66/5900951312038_A1N1_6812220_S13.png?sw=250&sh=250&sm=fit",
+    active: true,
+  },
+  {
+    id: "6847c9a7-40f6-4ad9-b79a-6bbecdd2a7a9",
+    name: "Cookies chocolat noisette",
+    brand: "Bonne Maman",
+    image:
+      "https://www.coursesu.com/dw/image/v2/BBQX_PRD/on/demandware.static/-/Sites-digitalu-master-catalog/default/dwed0f5587/3178530405781_A1L1_7037953_S12.png?sw=250&sh=250&sm=fit",
+    active: true,
+  },
+  {
+    id: "3b6faf3a-a99a-4860-9237-ff505fcbba42",
+    name: "Tartelettes au citron",
+    brand: "Bonne Maman",
+    image:
+      "https://www.coursesu.com/dw/image/v2/BBQX_PRD/on/demandware.static/-/Sites-digitalu-master-catalog/default/dw2e7e3f44/3178530402988_A1L1_2770003_S12.png?sw=250&sh=250&sm=fit",
+    active: true,
+  },
+  {
+    id: "02fd05e1-344a-4945-a99f-cf8b8aae1309",
+    name: "Biscuits sablés nappés au chocolat noir",
+    brand: "Granola",
+    image:
+      "https://www.coursesu.com/dw/image/v2/BBQX_PRD/on/demandware.static/-/Sites-digitalu-master-catalog/default/dwf93b347e/3017760595193_H1N1_4761417_S10.png?sw=250&sh=250&sm=fit",
+    active: true,
+  },
+  {
+    id: "1c9005fb-e165-4a5e-aabf-f1dd286f7d19",
+    name: "Tartelettes aux framboise",
+    brand: "Bonne Maman",
+    image:
+      "https://www.coursesu.com/dw/image/v2/BBQX_PRD/on/demandware.static/-/Sites-digitalu-master-catalog/default/dwfec36df1/3178530402995_A1L1_2782844_S12.png?sw=250&sh=250&sm=fit",
+    active: true,
+  },
+  {
+    id: "925848f2-befd-43af-bc95-bd8850777c56",
+    name: "Cookies extra choco et pépites de chocolat",
+    brand: "Granola",
+    image:
+      "https://www.coursesu.com/dw/image/v2/BBQX_PRD/on/demandware.static/-/Sites-digitalu-master-catalog/default/dwd2766b09/7622201495022_H1N1_6317814_S10.png?sw=250&sh=250&sm=fit",
+    active: true,
+  },
+  {
+    id: "fc24ff02-d570-40c9-8cdc-cc1b1f452d38",
+    name: "Biscuits petit déjeuner 'Le Tartiné' chocolat noisette Belvita",
+    brand: "LU",
+    image:
+      "https://www.coursesu.com/dw/image/v2/BBQX_PRD/on/demandware.static/-/Sites-digitalu-master-catalog/default/dw06a53941/7622300788094_H1N1_6300719_S10.png?sw=250&sh=250&sm=fit",
+    active: true,
+  },
 ];
-
-// {
-//   id: "",
-//   name: "",
-//   brand: "",
-//   image: ""
-// },
