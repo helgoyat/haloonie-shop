10993351b88a030c2e38e8bb76173f858a628f9f
Helene Goyat <goyat.helene@gmail.com>
2024-05-21 20:50:32 -0600

    One special box

--- src/data/boxes.ts
+++ src/data/boxes.ts
@@ -3,19 +3,16 @@ import { BoxTypeEnum, IBox } from "@/types";
 export const Boxes: IBox[] = [
   {
     id: "fae39da9-2601-4ba8-8382-62a867aa71d9",
-    name: "Test",
+    name: "Marie De France",
     type: BoxTypeEnum.Special,
-    image: "https://upload.wikimedia.org/wikipedia/en/0/0c/The_Genie_Aladdin.png",
+    image: "https://static.wikia.nocookie.net/disney/images/5/57/MarieCat.png",
     price: 48,
     cookies: {
-      "f50f9320-f33e-4ede-8582-c3500532f98c": 1,
-      "c2ae0379-9801-4e6c-a842-069fc78c4c49": 1,
-      "90d80965-c02f-4772-9e68-eedd864e0d2a": 1,
-      "8da70bfb-6539-4fe3-a21b-1281808d3a96": 1,
-      "523edc74-9902-4bea-8747-b8c91b3e9b6d": 1,
-      "e8f9491e-1ebc-45d7-9a61-5a094c23a60f": 1,
       "565db74a-a6bb-4938-8847-8486c6ce1380": 1,
-      "bc47b267-7b1f-4a53-9ee4-7b2a892002a5": 1,
+      "37310e59-9aee-4063-b852-23d9c9b44825": 1,
+      "126a8c12-d15f-44e8-b379-8689ab2af98c": 1,
+      "4a02eb7b-8096-400e-ba80-118b42598b4e": 1,
+      "83ee9497-ff94-48a1-a160-c4bac246c9dd": 1,
     },
   },
   {
