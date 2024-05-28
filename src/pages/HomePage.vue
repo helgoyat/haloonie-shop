e2f73442393dbe0148a2ee956f68c0848b62e739
Helene Goyat <goyat.helene@gmail.com>
2024-05-27 19:30:51 -0600

    Keep

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -12,17 +12,86 @@ import { BoxTypeEnum } from "@/types";
       src="https://cdn.pixabay.com/photo/2021/01/09/01/54/donut-5901143_1280.png" />
   </div>
   <div class="content !pt-2">
-    <h3>Welcome to you!</h3>
-    <div class="text-gray-500">
-      Track work across the enterprise through an open, collaborative platform. Link issues across
-      Jira and ingest data from other software development tools, so your IT support and operations
-      teams have richer contextual information to rapidly respond to requests, incidents, and
-      changes.
+    <hr-title title="Bonjour" />
+    <div class="text-gray-700 text-xl text-center">
+      Welcome to Haloonie, your destination for authentic French cookies and cakes, carefully boxed
+      and shipped to the USA for your enjoyment.
     </div>
-    <div class="text-gray-500">
-      Deliver great service experiences fast - without the complexity of traditional ITSM
-      solutions.Accelerate critical development work, eliminate toil, and deploy changes with ease,
-      with a complete audit trail for every change.
+    <div class="grid grid-cols-3 gap-6">
+      <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg">
+        <svg
+          class="w-8 h-8 text-violet-500"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          fill="none"
+          viewBox="0 0 24 24">
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="m8.032 12 1.984 1.984 4.96-4.96m4.55 5.272.893-.893a1.984 1.984 0 0 0 0-2.806l-.893-.893a1.984 1.984 0 0 1-.581-1.403V7.04a1.984 1.984 0 0 0-1.984-1.984h-1.262a1.983 1.983 0 0 1-1.403-.581l-.893-.893a1.984 1.984 0 0 0-2.806 0l-.893.893a1.984 1.984 0 0 1-1.403.581H7.04A1.984 1.984 0 0 0 5.055 7.04v1.262c0 .527-.209 1.031-.581 1.403l-.893.893a1.984 1.984 0 0 0 0 2.806l.893.893c.372.372.581.876.581 1.403v1.262a1.984 1.984 0 0 0 1.984 1.984h1.262c.527 0 1.031.209 1.403.581l.893.893a1.984 1.984 0 0 0 2.806 0l.893-.893a1.985 1.985 0 0 1 1.403-.581h1.262a1.984 1.984 0 0 0 1.984-1.984V15.7c0-.527.209-1.031.581-1.403Z" />
+        </svg>
+        <h5 class="my-2 text-2xl font-semibold text-gray-900">Our Mission</h5>
+        <p class="mb-3 font-normal text-gray-500">
+          We aim to share French cookies and cakes across the Atlantic, offering a taste of France
+          in every bite.
+        </p>
+        <p class="mb-3 font-normal text-gray-500">
+          Our commitment to quality, and customer satisfaction ensures that every box is filled with
+          love and care.
+        </p>
+      </div>
+      <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg">
+        <svg
+          class="w-8 h-8 text-violet-500"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          fill="none"
+          viewBox="0 0 24 24">
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M12.01 6.001C6.5 1 1 8 5.782 13.001L12.011 20l6.23-7C23 8 17.5 1 12.01 6.002Z" />
+        </svg>
+        <h5 class="my-2 text-2xl font-semibold text-gray-900">Enjoy</h5>
+        <p class="mb-3 font-normal text-gray-500">
+          Our boxes are perfect for treating yourself or gifting someone special.
+        </p>
+        <p class="mb-3 font-normal text-gray-500">
+          From classic French cookies and madeleines to decadent sablés and galettes, our selection
+          of treats is carefully crafted so you can experience the authentic taste of France without
+          leaving the USA.
+        </p>
+      </div>
+      <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg">
+        <svg
+          class="w-8 h-8 text-violet-500"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          fill="none"
+          viewBox="0 0 24 24">
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M13 7h6l2 4m-8-4v8m0-8V6a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v9h2m8 0H9m4 0h2m4 0h2v-4m0 0h-5m3.5 5.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Zm-10 0a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Z" />
+        </svg>
+        <h5 class="my-2 text-2xl font-semibold text-gray-900">Shipping</h5>
+        <p class="mb-3 font-normal text-gray-500">
+          We ship our boxes to designated FedEx Pickup Offices across the USA, where you can
+          conveniently collect your package at your earliest convenience.
+        </p>
+      </div>
     </div>
     <div class="flex flex-col items-center my-2">
       <router-link :to="{ name: 'HowItWorksPage' }">
